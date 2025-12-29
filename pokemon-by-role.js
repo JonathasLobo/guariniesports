6d6ac43aaf0c941cacdf9f58284467cc428a0fc7
Geoshad35 <geoshadsccp@gmail.com>
2025-12-29 18:36:48 -0300

    meta 29/12

--- pokemon-by-role.js
+++ pokemon-by-role.js
@@ -84,8 +84,8 @@ function normalizeForImage(name) {
 // Carregar dados do meta
 async function loadMetaData() {
   try {
-    console.log('🔍 Buscando arquivo meta22-12-2025.json...');
-    const response = await fetch('./meta22-12-2025.json');
+    console.log('🔍 Buscando arquivo meta29-12-2025.json...');
+    const response = await fetch('./meta29-12-2025.json');
     
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
